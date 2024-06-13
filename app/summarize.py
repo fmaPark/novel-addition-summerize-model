4a19c4c24cec1528f11d6eb66667d7f2f9d0c981
pma <pma@pmaui-MacBookPro.local>
2024-06-13 10:04:45 +0900

    change model

--- app/summarize.py
+++ app/summarize.py
@@ -7,6 +7,7 @@ def summarize(text: str) -> str:
         text_input = text
         inputs = tokenizer.encode("summarize: " + text_input, return_tensors="pt", max_length=1024, truncation=True)
 
+        # summary_ids = model.generate(inputs, max_new_tokens=20, num_beams=5, no_repeat_ngram_size=2, early_stopping=True)
         summary_ids = model.generate(inputs, max_length=150, num_beams=2, early_stopping=True)
         summary_text = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
         return summary_text
